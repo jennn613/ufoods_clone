742cd6d78db682ce5683c8d57aef067066831270
Jenny <69027218+jennn613@users.noreply.github.com>
2020-11-19 18:03:47 +0900

    add js file

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function mediaSize() { 
+    if ( window.matchMedia('(min-width: 320px) and (max-width: 767px)').matches ) {
+        // console.log('Mobile');
+    } else if ( window.matchMedia('(min-width: 768px) and (max-width: 1024px)').matches ) {
+        // console.log('Tablet');
+    } else {
+        // console.log('PC');
+    }
+};
+
+
+window.addEventListener('resize', mediaSize);
+
+mediaSize();
