7281715d7a0c9ef08a7c085c97b1a8bc9811a260
Jenny <69027218+jennn613@users.noreply.github.com>
2020-12-28 12:22:52 +0900

    delete contact file

--- js/ufoods.js
+++ js/ufoods.js
@@ -4,45 +4,6 @@ import { toggleClick } from "./mobile.js";
 import { headerMenuShow } from "./tablet.js";
 import { headerLogoShow, contentsFadeIn, homeIntroMove, homeImgShow, imgBtnMove, toTheTop } from "./pc.js";
 
-// function mediaSize() { 
-//     if ( window.matchMedia('(min-width: 320px) and (max-width: 767px)').matches ) {
-//         // console.log('Mobile');
-//         toggleClick();
-//         headerLogoShow();
-//         contentsFadeIn();
-//         homeImgShow();
-//         homeIntroMove();
-//         imgBtnMove();
-//         toTheTop();
-//     } else if ( window.matchMedia('(min-width: 768px) and (max-width: 1024px)').matches ) {
-//         // console.log('Tablet');
-//         headerLogoShow();
-//         contentsFadeIn();
-//         homeImgShow();
-//         homeIntroMove();
-//         imgBtnMove();
-//         toTheTop();
-//     } else {
-//         // console.log('PC');
-//         headerLogoShow();
-//         contentsFadeIn();
-//         homeImgShow();
-//         homeIntroMove();
-//         imgBtnMove();
-//         toTheTop();
-//         //aFocus();
-//     }
-// };
-
-
-// window.addEventListener('resize', mediaSize);
-
-// mediaSize();
-
-// console.log( toTheTop );
-
-
-
 const deviceSize = [
     { type: 'mobile', size: 767 },
     { type: 'tablet', size: 1024 },
@@ -75,7 +36,6 @@ const Resize = function () {
             contentsFadeIn();
             homeImgShow();
             homeIntroMove();
-            homeImgShow();
             imgBtnMove();
             toTheTop();
         }
